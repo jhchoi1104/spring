93c45319b28909d6bba48272584c43fb64de0b03
jhchoi1104 <163481864+jhchoi1104@users.noreply.github.com>
2024-12-17 16:37:23 +0900

    add: Spring 예제 2

--- core/src/test/java/hello/core/member/MemberServiceImplTest.java
+++ core/src/test/java/hello/core/member/MemberServiceImplTest.java
@@ -1,13 +1,21 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceImplTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
@@ -16,7 +24,7 @@ class MemberServiceImplTest {
 
         // when
         memberService.join(member);
-        Member findMember = memberService.findMember(2L);
+        Member findMember = memberService.findMember(1L);
 
         // then
         Assertions.assertThat(member).isEqualTo(findMember);
